3e84c716d2890ab8622100e6eb3426b9812ef3d1
Munif Tanjim <hello@muniftanjim.dev>
2022-01-27 13:39:50 +0600

    fix: code_actions fix range for same row

--- lua/eslint/utils.lua
+++ lua/eslint/utils.lua
@@ -59,10 +59,12 @@ local function get_fix_range(problem, params)
   local end_col
   if start_offset == end_offset then
     end_col = start_col
-  elseif start_row == end_row then
-    end_col = start_col + end_col
   else
     end_col = #lines_from_start_offset_to_end_offset[#lines_from_start_offset_to_end_offset]
+
+    if start_row == end_row then
+      end_col = start_col + end_col
+    end
   end
 
   return { row = start_row, col = start_col, end_row = end_row, end_col = end_col }
